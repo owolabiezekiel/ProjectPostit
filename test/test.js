c3118bcbbc12df456d55104f981f85d9f4c2062d
Owolabi <owo.ezekiel@gmail.com>
2017-07-14 08:40:00 +0100

    wrote tests for sign-up and sign-in API routes

--- test/test.js
+++ test/test.js
@@ -1,3 +1,107 @@
-let expect = require('chai').expect;
+process.env.NODE_ENV = 'test';
+const users = require('../server/models/users');
 
-const Users = require('../server/controllers/user.js').;
+//Require the dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app.js');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('status 200', () => {
+      it('it should POST a  user', (done) => {
+        const user = {
+            username: "oooooooooooo",
+            email: "akiiii30@gmail.com",
+            password: "ewa"
+        }
+        chai.request(app)
+            .post('/api/users/signup')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message').eql('You are registered Successfully');
+              done();
+            });
+      });
+
+  });
+
+describe('status 400', () => {
+      it('it should POST a  user', (done) => {
+        const user = {
+            username: "oooooooooooo",
+            email: "akiiiiiiii30@gmail.com",
+            password: "ewa"
+        }
+        chai.request(app)
+            .post('/api/users/signup')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message').eql('Username already exist');
+              done();
+            });
+      });
+
+  });
+
+describe('status 400', () => {
+      it('it should POST a  user', (done) => {
+        const user = {
+            username: "oooooooooooooooooooooooooo",
+            email: "akiiii30@gmail.com",
+            password: "ewa"
+        }
+        chai.request(app)
+            .post('/api/users/signup')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message').eql('email already exist');
+              done();
+            });
+      });
+
+  });
+
+ describe('status 201', () => {
+    it('successfully logged in', (done) => {
+      const user = {
+        username: 'oooooooooooo',
+        password: 'ewa'
+      };
+      chai.request(app)
+        .post('/api/users/signin')
+          .send(user)
+          .end((err, res) => {
+            res.status.should.equal(201);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message').eql('logged in successfully');
+            done();
+          });
+
+    });
+});
+
+describe('status 404', () => {
+    it('throws error for invalid user', (done) => {
+      const user = {
+        username: 'hhhhhhhhhhhhhhhhhhhhhhhhh',
+        password: 'ewa'
+      };
+      chai.request(app)
+        .post('/api/users/signin')
+          .send(user)
+          .end((err, res) => {
+            res.status.should.equal(404);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message').eql('User not found');
+            done();
+          });
+    });
+});
